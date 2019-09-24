afeda0337f5b0c3105190be2bf81c3f11381ff68
Doug Kintop <dkintop@hotmail.com>
2019-09-24 16:18:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
+ require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver 
+    @amount = amount
+    @status = "pending"
+  end 
+  
+  def valid? 
+    if sender.valid? && receiver.valid?
+      true 
+    else 
+      false
+    end
+    #binding.pry
+  end 
+  
+  def execute_transaction
+    if @status == "pending" && @sender.balance > @amount && @receiver.valid?
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+      #binding.pry
+    else
+       @status = "rejected"
+       return "Transaction rejected. Please check your account balance."
+    end
+  end 
+  
 end
+ 
